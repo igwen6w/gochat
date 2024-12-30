3b7b260d6bb5e4a8fcedb3d8e628cb7dd53c0bca
igwen6w <igwen6w@gmail.com>
2024-12-30 23:58:00 +0800

    增加超时下线功能

--- user.go
+++ user.go
@@ -6,24 +6,31 @@ import (
 	"net"
 	"runtime"
 	"strings"
+	"sync"
+	"time"
 )
 
 type User struct {
-	Name   string
-	Addr   string
-	C      chan string
-	Conn   net.Conn
-	Server *Server
+	Name        string
+	Addr        string
+	C           chan string
+	Conn        net.Conn
+	Server      *Server
+	LastActive  time.Time     // 最后活动时间
+	done        chan struct{} // 用于通知goroutine退出
+	offlineOnce sync.Once     // 确保下线操作只执行一次
 }
 
 func newUser(conn net.Conn, server *Server) *User {
 	userAddr := conn.RemoteAddr().String()
 	user := &User{
-		Name:   userAddr,
-		Addr:   userAddr,
-		C:      make(chan string),
-		Conn:   conn,
-		Server: server,
+		Name:       userAddr,
+		Addr:       userAddr,
+		C:          make(chan string),
+		Conn:       conn,
+		Server:     server,
+		LastActive: time.Now(),
+		done:       make(chan struct{}),
 	}
 
 	// 接收显示消息
@@ -32,23 +39,51 @@ func newUser(conn net.Conn, server *Server) *User {
 	// 监听消息和指令
 	go user.ListenMessage()
 
+	// 启动定时检查
+	go user.CheckActivity()
+
 	return user
+}
+
+func (u *User) CheckActivity() {
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			if time.Since(u.LastActive) > 10*time.Second {
+				u.OffLine()
+				return
+			}
+		case <-u.done:
+			return
+		}
+	}
 }
 
 func (u *User) ListenMessage() {
 	buf := make([]byte, 1024)
 	for {
-		n, err := u.Conn.Read(buf)
-		if err != nil && err != io.EOF {
-			fmt.Println("conn.read err:", err)
+		select {
+		case <-u.done:
 			return
-		}
+		default:
+			n, err := u.Conn.Read(buf)
+			if err != nil && err != io.EOF {
+				fmt.Println("conn.read err:", err)
+				u.OffLine()
+				return
+			}
 
-		if n == 0 {
-			u.OffLine()
-		}
+			if n == 0 {
+				u.OffLine()
+				return
+			}
 
-		u.SendMessage(string(buf[:n-1]))
+			u.LastActive = time.Now()
+			u.SendMessage(string(buf[:n-1]))
+		}
 	}
 }
 
@@ -64,6 +99,7 @@ func (u *User) SendMessage(msg string) {
 		u.C <- "Pong"
 	} else if msg == "offline" || msg == "exit" {
 		u.OffLine()
+		return
 	} else if len(msg) >= 8 && msg[0:7] == "rename|" {
 		// 用户重命名，格式：rename|string
 		u.Rename(msg[7:])
@@ -84,21 +120,33 @@ func (u *User) SendMessage(msg string) {
 // ShowMessage 显示消息
 func (u *User) ShowMessage() {
 	for {
-		message := <-u.C
-
-		u.Conn.Write([]byte(fmt.Sprintf("%s\n", message)))
+		select {
+		case message, ok := <-u.C:
+			if !ok {
+				return
+			}
+			u.Conn.Write([]byte(fmt.Sprintf("%s\n", message)))
+		case <-u.done:
+			return
+		}
 	}
 }
 
 // OffLine 用户下线
 func (u *User) OffLine() {
-	u.Conn.Close()
+	u.offlineOnce.Do(func() {
+		close(u.done) // 通知所有goroutine退出
 
-	u.Server.DeleteOnlineMap(u)
+		u.Server.DeleteOnlineMap(u)
 
-	u.Server.PushMessage(u, "下线了")
+		u.Server.PushMessage(u, "下线了")
 
-	runtime.Goexit()
+		close(u.C)
+
+		u.Conn.Close()
+
+		runtime.Goexit()
+	})
 }
 
 func (u *User) Rename(newName string) {
